6075bf4a85c2531d801446c5bd0f48990141f302
TimLohrer <me@timlohrer.de>
2026-04-30 00:10:10 +0200

    amend: implement basic ws client

---
+++ src/lib/models/WebsocketEvent.ts
@@ -0,0 +1,9 @@
+export class WebsocketEvent {
+    static Ready = 'Ready';
+    static Disconnect = 'Disconnect';
+    static CreateGame = 'CreateGame';
+    static JoinGame = 'JoinGame';
+    static GameJoined = 'GameJoined';
+    static PlayerJoined = 'PlayerJoined';
+    static PlayerDisconnected = 'PlayerDisconnected';
+}
