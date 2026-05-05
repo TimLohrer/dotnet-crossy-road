b565669558779f43b7db67778eef0771fac11ce4
lip1we <richard.v.lippmann@gmail.com>
2026-05-05 17:06:05 +0200

    feat: add death screen component

---
+++ src/lib/DeathScreen.svelte
@@ -0,0 +1,51 @@
+<div class="container">
+    <div class="deathmessage">
+        You Died
+    </div>
+    <button>Restart</button>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        flex-direction: column;
+        z-index: 102;
+        backdrop-filter: blur(10px);
+        gap: 4rem;
+    }
+
+    .deathmessage {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 4rem;
+        color: red;
+        height: 10rem;
+        width: 50rem;
+        background-color: black;
+        border-style: solid;
+        border-width: 0.4rem;
+        border-color: white;
+    }
+
+    button {
+        outline: none;
+        background-color: rgb(0, 0, 0);
+        color: white;
+        height: 4rem;
+        width: 10rem;
+        font-family: PixelFont;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        border-style: solid;
+        border-width: 0.4rem;
+        border-color: white;
+    }
+</style>
