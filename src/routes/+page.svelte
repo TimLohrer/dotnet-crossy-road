14ef9e91cf53adb56c69a8afd64652de7809aa70
lip1we <richard.v.lippmann@gmail.com>
2026-05-05 10:29:12 +0200

    feat: add conditional rendering for PopUp based on menuState

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,17 @@
 	import Menu from "$lib/Menu.svelte";
 	import CrossyRoadGame from "$lib/CrossyRoadGame.svelte";
 	import GameHud from "$lib/GameHud.svelte";
+	import { MenuState } from "$lib/models/MenuState";
+	import { menuState } from "$lib/stores/stateStore";
+	import PopUp from "$lib/ PopUp.svelte";
+
+
 </script>
+
 
 <Menu />
 <CrossyRoadGame />
 <GameHud />
+{#if $menuState === MenuState.Skins}
+	<PopUp />
+{/if}
