04d453b3f5c5c8f68bec123f09069989f2297993
lip1we <richard.v.lippmann@gmail.com>
2026-05-05 10:32:21 +0200

    feat: add PopUp component with styling and layout

---
+++ src/lib/ PopUp.svelte
@@ -0,0 +1,27 @@
+<div class="container">
+    <div class="popup"></div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        z-index: 100;
+        position: absolute;
+        backdrop-filter: blur(0px);
+        pointer-events: none;
+    }
+
+    .popup {
+        width: 40rem;
+        height: 45rem;
+        background-color: rgb(0, 0, 0);
+        box-shadow: rgb(255, 255, 255) 7px 7px 0px;   
+        margin-bottom: 7rem;
+        pointer-events: all;
+    }
+
+</style>
