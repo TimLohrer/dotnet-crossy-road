90b382d276064bea44b82a34b9b822a370f64847
TimLohrer <me@timlohrer.de>
2026-05-05 20:26:36 +0200

    feat: only load user in main page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,34 +1,13 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-	import { PUBLIC_API_URL } from '$lib/environment.js';
-	import type { User } from '$lib/models/User.js';
-	import { user as userStore } from '$lib/stores/stateStore.js';
-	import { onMount } from 'svelte';
 
 	const { children } = $props();
-
-	onMount(async () => {
-		const res = await fetch(`${PUBLIC_API_URL}/user/@me`, {
-			credentials: 'include',
-			headers: {
-				cookie: document.cookie
-			}
-		});
-
-		if (!res.ok) {
-			throw window.location.replace(`${PUBLIC_API_URL}/auth/signin/bosch`);
-		}
-
-		const user = await res.json() as User;
-
-		userStore.update(() => user);
-	});
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
- 
+
 {@render children()}
 
 <style>
@@ -41,5 +20,8 @@
 
 	:global(*) {
 		font-family: PixelFont !important;
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
 	}
 </style>
