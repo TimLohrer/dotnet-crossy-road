71e8b63b68fb968df095216dc6cbf6a9fca3be39
Tim Lohrer <vht1fe@bosch.com>
2026-05-05 10:23:30 +0200

    chore: remove dummy user

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,7 @@
 <script lang="ts">
 	import Menu from "$lib/Menu.svelte";
 	import CrossyRoadGame from "$lib/CrossyRoadGame.svelte";
-	import { user } from "$lib/stores/stateStore";
-	import { Skin } from "$lib/models/Skin";
 	import GameHud from "$lib/GameHud.svelte";
-
-	$user = {
-		id: crypto.randomUUID(),
-		username: `Player ${Math.random().toString(36).substring(2, 8)}`,
-		highscore: 0,
-		taler: 0,
-		skin: Skin.Default,
-		ownedSkins: [Skin.Default]
-	}
 </script>
 
 <Menu />
