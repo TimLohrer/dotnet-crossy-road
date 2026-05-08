8a79770e42f8ac8d5b5ce2c0737a83adb2832aa5
TimLohrer <me@timlohrer.de>
2026-05-08 21:23:36 +0200

    fix: actually make players transparent on start

--- src/lib/GameSocket.ts
+++ src/lib/GameSocket.ts
@@ -180,6 +180,8 @@ export class GameSocket {
 		});
 
 		this.connection.on(WebsocketEvent.UpdateUser, (updatedUser: User) => userStore.update(() => updatedUser));
+
+		this.connection.on(WebsocketEvent.StartGame, () => this.getRenderer()?.makeOtherPlayersTransparent());
 	}
 
 	public async connect() {
