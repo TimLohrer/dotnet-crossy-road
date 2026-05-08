08cac121e645ad5393c232c357e2e4f182b6a090
lip1we <richard.v.lippmann@gmail.com>
2026-05-08 09:13:41 +0200

    feat: remove debug logs from player death conditions and add leaderboard component

--- src/lib/GameRenderer.ts
+++ src/lib/GameRenderer.ts
@@ -636,15 +636,11 @@ export class GameRenderer {
 		const elementAtPos = this.getElementAtPosition(currentPosition);
 
 		if (isActiveUser && collidableElementAtPos) {
-			console.log("collide");
-			
 			this.sendPlayerDeathOnce();
 			return;
 		}
 
 		if (isActiveUser && performance.now() - this.lastMoveTime >= GameRenderer.IDLE_DEATH_TIME_MS && player.score > 0) {
-			console.log("time");
-			
 			this.sendPlayerDeathOnce();
 			return;
 		}
@@ -656,8 +652,6 @@ export class GameRenderer {
 				currentPosition.x > 6 ||
 				currentPosition.x < -6)
 		) {
-			console.log("water");
-			
 			this.sendPlayerDeathOnce();
 			return;
 		}
