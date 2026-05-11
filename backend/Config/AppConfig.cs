57086f1d2d9fefc060d9122135a510aaba588f69
Tim Lohrer <vht1fe@bosch.com>
2026-05-11 11:25:39 +0200

    feat: remove unused oauth o.o

--- backend/Config/AppConfig.cs
+++ backend/Config/AppConfig.cs
@@ -3,7 +3,6 @@ namespace CrossyRoadApi.Config;
 public class AppConfig
 {
     public DatabaseConfig Database { get; set; }
-    public OAuthConfig BoschOAuth { get; set; }
     public OAuthConfig MicrosoftOAuth { get; set; }
     public string? Domain { get; set; }
 }
