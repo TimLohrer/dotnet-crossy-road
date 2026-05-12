d2e5dec4553f2d89c2b1c18d67155345052bbeac
Tim Lohrer <vht1fe@bosch.com>
2026-05-12 10:50:18 +0200

    chore: rename LeaderBoard to Leaderboard

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -9,7 +9,7 @@
     }
 
     function lookupLeaderboard() {
-        $menuState = MenuState.LeaderBoard;
+        $menuState = MenuState.Leaderboard;
     }
 </script>
 
--- src/lib/models/MenuState.ts
+++ src/lib/models/MenuState.ts
@@ -3,5 +3,5 @@ export enum MenuState {
     Play,
     JoinGame,
     Seed,
-    LeaderBoard,
+    Leaderboard,
 }
